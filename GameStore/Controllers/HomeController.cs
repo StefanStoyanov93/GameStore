c3016b03ccdf0576b389bc63c9af891a32f1b065
StefanStoyanov93 <stefanstoqnov93@gmail.com>
2022-03-16 20:39:33 +0200

    ading Store controller

--- GameStore/Controllers/HomeController.cs
+++ GameStore/Controllers/HomeController.cs
@@ -13,20 +13,12 @@ namespace GameStore.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+        public IActionResult Index() => View();
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        public IActionResult Privacy() => View();
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+            => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
---
+++ GameStore/Controllers/StoreController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GameStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        public IActionResult Games() => View();
+    }
+}
