1eb1702164c89e288876035e0aee0edec3078270
StefanStoyanov93 <stefanstoqnov93@gmail.com>
2022-04-06 23:52:18 +0300

    start the initial detail view but needs more work on the front end.

--- GameStore/Controllers/AppController.cs
+++ GameStore/Controllers/AppController.cs
@@ -1,8 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GameStore.Core.Serveces.Contracts;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GameStore.Controllers
 {
     public class AppController : Controller
     {
+        private readonly IGameService gameService;
+
+        public AppController(IGameService _gameService)
+        {
+            this.gameService = _gameService;
+        }
+
+        public IActionResult Details(string id)
+        {
+            var game = gameService.Details(id);
+
+            return View(game);
+        }
     }
 }
